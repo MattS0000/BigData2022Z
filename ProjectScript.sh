5216983c3a8ddc2c106a239a81b0dd104a0b88f6
Kinga Ułasik <56126275+ARUMHC@users.noreply.github.com>
2023-01-12 19:29:02 +0100

    changing violation_date to varchar(50)

--- ProjectScript.sh
+++ ProjectScript.sh
@@ -3,7 +3,7 @@ hdfs dfs -mkdir /user/project/input
 hdfs dfs -mkdir /user/project/input/crashes
 hdfs dfs -mkdir /user/project/input/violations
 
-hive -e 'create table speed_camera_violations(address varchar(250),camera_id varchar(50),violation_date date,violations int,x_cord float,y_cord float,latitude float,longitude float) row format delimited;'
+hive -e 'create table speed_camera_violations(address varchar(250),camera_id varchar(50),violation_date varchar(50), violations int,x_cord float,y_cord float,latitude float,longitude float) row format delimited;'
 
 
 /usr/local/hbase/bin/hbase shell
